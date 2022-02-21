f5de6c84f35a7f6264752bbff134c45e04489efb
Conor White <Conor.White@liberty-it.co.uk>
2022-02-21 14:42:06 +0000

    Template components

--- Training-Project/src/app/app.module.ts
+++ Training-Project/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { UploadDocumentComponent } from './components/upload-document/upload-document.component';
+import { ScanDocumentComponent } from './components/scan-document/scan-document.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { FinalPageComponent } from './components/final-page/final-page.component';
+import { PopUpWindowComponent } from './components/pop-up-window/pop-up-window.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    UploadDocumentComponent,
+    ScanDocumentComponent,
+    ConfirmationComponent,
+    FinalPageComponent,
+    PopUpWindowComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ Training-Project/src/app/components/scan-document/scan-document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scan-document',
+  templateUrl: './scan-document.component.html',
+  styleUrls: ['./scan-document.component.css']
+})
+export class ScanDocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
