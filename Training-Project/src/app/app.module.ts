5ff0379333e0a07aec115a8f6907a377843b483b
Anas Alagtal <anas.alagtal@liberty-it.co.uk>
2022-02-24 16:30:15 +0000

    branch with initial qrcode feature

--- Training-Project/src/app/app.module.ts
+++ Training-Project/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FinalPageComponent } from './components/final-page/final-page.component
 import { PopUpWindowComponent } from './components/pop-up-window/pop-up-window.component';
 import { WebcamModule} from "ngx-webcam";
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
-
+import { QRCodeModule } from 'angularx-qrcode'; //This is for QR code
 
 
 @NgModule({
@@ -26,7 +26,8 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WebcamModule
+    WebcamModule,
+    QRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
